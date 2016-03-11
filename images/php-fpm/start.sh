031f2d26dbbf34077e74e80714996689b4486101
Jorge Zapata <jorge.guridi@globant.com>
2016-03-10 18:10:33 -0600

    Refactoring images and creating a new one

--- images/php-fpm/start.sh
+++ images/php-fpm/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sed -i "s@listen = /var/run/php5-fpm.sock@listen = 9000@" /etc/php5/fpm/pool.d/www.conf
 
+echo "xdebug.remote_enable=1" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_port=9001" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_host=172.17.0.1" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 1000" >> /etc/php5/mods-available/xdebug.ini
+
 exec /usr/sbin/php5-fpm --nodaemonize
