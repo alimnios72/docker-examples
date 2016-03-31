d602549305174cf90876a4e6b4f7fecdc05a6a35
Jorge Zapata Guridi <jorge.guridi@globant.com>
2016-03-30 18:14:11 -0600

    Missing debug param

--- images/php-fpm/start.sh
+++ images/php-fpm/start.sh
@@ -5,5 +5,6 @@ echo "xdebug.remote_enable=1" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_port=9001" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.remote_host=172.17.0.1" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.max_nesting_level = 1000" >> /etc/php5/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php5/mods-available/xdebug.ini
 
 exec /usr/sbin/php5-fpm --nodaemonize
